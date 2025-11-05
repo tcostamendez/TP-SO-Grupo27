57034b544bee551aac6688e31346c48f0666355c
Pablo-Goros <pgorostiaga@DESKTOP-OIE8P4Q.localdomain>
2025-11-05 18:14:16 -0300

    feat: Agregue el comando loop (No funciona como esperado)

--- TP2-SO-2025/Kernel/format_test.c
+++
@@ -1,5 +0,0 @@
-int main(void){
-    if(2!=NULL){
-        printf("2 is not NULL\n");
-    }return 0;
-}
--- TP2-SO-2025/Userland/Shell/commands/loop.c
+++ TP2-SO-2025/Userland/Shell/commands/loop.c
@@ -1,3 +1,28 @@
+#include "stdio.h"
+#include "libsys.h"
+
+
 int _loop(int argc, char *argv[]) {
+    if (argc != 2) {
+		perror("Usage: loop <miliseconds>\n");
+		return 1;
+	}
+
+    int milliseconds = 0;
+
+    if (argv != NULL && argv[1] != NULL && (sscanf(argv[1], "%d", &milliseconds) != 1 || milliseconds <= 0)) {
+		perror("Invalid time interval\n");
+		return 1;
+	}
+
+    int pid = getMyPid();
+
+    while (1) {
+		// Note: although getpid() could be called just once
+		// it is called every time to demonstrate that the process ID does not change
+		printf("Hello from PID: %d\n", pid);
+		sleep(milliseconds);
+	}
+
     return 0;
 }
