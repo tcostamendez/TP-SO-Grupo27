efb759fb601e5677fc33bcc09e982ac3921d97a1
santiagoRapan <srapan@itba.edu.ar>
2025-11-12 10:47:54 -0300

    final commit

--- TP2-SO-2025/Kernel/sem.c
+++ TP2-SO-2025/Kernel/sem.c
@@ -1,7 +1,3 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "sem.h"
 #include "interrupts.h"
 #include "process.h"
@@ -11,311 +7,356 @@ extern void semLock(uint8_t *lock);
 extern void semUnlock(uint8_t *lock);
 extern void _force_scheduler_interrupt();
 
-static semQueue *sQueue = NULL;
+// Global semaphore registry
+static semQueue *global_semaphore_registry = NULL;
 
-static Sem findSemByName(const char *name);
-static int validSem(Sem semToValid);
-
-static int cmpPid(void *pid1, void *pid2);
-static int cmpSem(void *psem1, void *psem2);
+// Forward declarations for helper functions
+static Sem locate_semaphore_by_name(const char *semaphore_name);
+static int is_semaphore_valid(Sem target_semaphore);
+static int compare_process_ids(void *pid_a, void *pid_b);
+static int compare_semaphore_pointers(void *sem_a_ptr, void *sem_b_ptr);
 
 
 int initSemQueue(void) {
-    if (sQueue != NULL) {
+    if (global_semaphore_registry != NULL) {
         panic("Semaphore queue already initialized");
     }
-    sQueue = mm_alloc(sizeof(semQueue));
-    if (sQueue == NULL) {
+    
+    global_semaphore_registry = mm_alloc(sizeof(semQueue));
+    if (global_semaphore_registry == NULL) {
         panic("Failed to allocate memory for semaphore queue");
     }
-    sQueue->lock = 0;
-    sQueue->sems = createQueue(cmpSem, sizeof(struct sem *));
-    return sQueue->sems == NULL ? -1 : 1;
+    
+    global_semaphore_registry->lock = 0;
+    global_semaphore_registry->sems = createQueue(compare_semaphore_pointers, sizeof(struct sem *));
+    
+    return (global_semaphore_registry->sems == NULL) ? -1 : 1;
 }
 
 void freeSemQueue(void) {
-	if (sQueue == NULL) {
-		return;
-	}
-	semLock(&sQueue->lock);
-	if (sQueue->sems != NULL) {
-		while (queueSize(sQueue->sems) > 0) {
-			Sem s = NULL;
-			if (dequeue(sQueue->sems, &s) != NULL && s != NULL) {
-				if (s->blockedProcesses)
-					queueFree(s->blockedProcesses);
-				mm_free(s);
-			}
-		}
-		queueFree(sQueue->sems);
-	}
-	semUnlock(&sQueue->lock);
-	mm_free(sQueue);
-	sQueue = NULL;
+    if (global_semaphore_registry == NULL) {
+        return;
+    }
+    
+    semLock(&global_semaphore_registry->lock);
+    
+    if (global_semaphore_registry->sems != NULL) {
+        while (queueSize(global_semaphore_registry->sems) > 0) {
+            Sem semaphore_entry = NULL;
+            if (dequeue(global_semaphore_registry->sems, &semaphore_entry) != NULL && 
+                semaphore_entry != NULL) {
+                if (semaphore_entry->blockedProcesses != NULL) {
+                    queueFree(semaphore_entry->blockedProcesses);
+                }
+                mm_free(semaphore_entry);
+            }
+        }
+        queueFree(global_semaphore_registry->sems);
+    }
+    
+    semUnlock(&global_semaphore_registry->lock);
+    mm_free(global_semaphore_registry);
+    global_semaphore_registry = NULL;
 }
 
 Sem semOpen(const char *name, uint16_t value) {
     if (name == NULL || strlen(name) >= MAX_SEM_LENGTH) {
         return NULL;
-    } 
+    }
     
-    Sem newSem;
-
-	if (sQueue == NULL) {
-		return NULL;
-	}
-
-	semLock(&sQueue->lock);
-
-    if (!queueIsEmpty(sQueue->sems)) {
-        newSem = findSemByName(name);
-        if (newSem) {
-			semLock(&(newSem->lock));
-			(newSem->users)++;
-			semUnlock(&(newSem->lock));
-			semUnlock(&(sQueue->lock));
-			return newSem;
-		}
-    }
-
-    newSem = mm_alloc(sizeof(sem));
-    if (newSem == NULL) {
-        semUnlock(&sQueue->lock);
+    if (global_semaphore_registry == NULL) {
         return NULL;
     }
 
-    my_strcpy(newSem->name, name);
-    newSem->value = value;
-    newSem->lock = 0;
-    newSem->users = 1;
-    newSem->blockedProcesses = createQueue(cmpPid, sizeof(int));
-    if (newSem->blockedProcesses == NULL) {
-        mm_free(newSem);
-        semUnlock(&sQueue->lock);
+    semLock(&global_semaphore_registry->lock);
+
+    // Check if semaphore with this name already exists
+    Sem existing_semaphore = NULL;
+    if (!queueIsEmpty(global_semaphore_registry->sems)) {
+        existing_semaphore = locate_semaphore_by_name(name);
+        if (existing_semaphore != NULL) {
+            semLock(&existing_semaphore->lock);
+            existing_semaphore->users++;
+            semUnlock(&existing_semaphore->lock);
+            semUnlock(&global_semaphore_registry->lock);
+            return existing_semaphore;
+        }
+    }
+
+    // Create a new semaphore
+    Sem new_semaphore = mm_alloc(sizeof(sem));
+    if (new_semaphore == NULL) {
+        semUnlock(&global_semaphore_registry->lock);
         return NULL;
     }
 
-    enqueue(sQueue->sems, &newSem);
-    semUnlock(&sQueue->lock);
+    my_strcpy(new_semaphore->name, name);
+    new_semaphore->value = value;
+    new_semaphore->lock = 0;
+    new_semaphore->users = 1;
+    new_semaphore->blockedProcesses = createQueue(compare_process_ids, sizeof(int));
+    
+    if (new_semaphore->blockedProcesses == NULL) {
+        mm_free(new_semaphore);
+        semUnlock(&global_semaphore_registry->lock);
+        return NULL;
+    }
 
-    return newSem;
+    enqueue(global_semaphore_registry->sems, &new_semaphore);
+    semUnlock(&global_semaphore_registry->lock);
+
+    return new_semaphore;
 }
 
-int semClose(Sem semToClose) {
-	if (sQueue == NULL) {
-		return -1;
-	}
-	if (!validSem(semToClose)) {
-		return -1;
-	}
+int semClose(Sem target_semaphore) {
+    if (global_semaphore_registry == NULL) {
+        return -1;
+    }
+    
+    if (!is_semaphore_valid(target_semaphore)) {
+        return -1;
+    }
 
-	semLock(&sQueue->lock);
-	semLock(&semToClose->lock);
+    semLock(&global_semaphore_registry->lock);
+    semLock(&target_semaphore->lock);
 
-	semToClose->users--;
+    target_semaphore->users--;
 
-	if (semToClose->users != 0) {
-		semUnlock(&semToClose->lock);
-		semUnlock(&sQueue->lock);
-		return 0;
-	}
+    // If other processes are still using this semaphore, don't remove it
+    if (target_semaphore->users != 0) {
+        semUnlock(&target_semaphore->lock);
+        semUnlock(&global_semaphore_registry->lock);
+        return 0;
+    }
 
-	Sem aux = semToClose;
-	if (queueRemove(sQueue->sems, &aux) == NULL) {
-		semToClose->users++;
-		semUnlock(&semToClose->lock);
-		semUnlock(&sQueue->lock);
-		return -1;
-	}
+    // Remove semaphore from registry
+    Sem semaphore_to_remove = target_semaphore;
+    if (queueRemove(global_semaphore_registry->sems, &semaphore_to_remove) == NULL) {
+        target_semaphore->users++;
+        semUnlock(&target_semaphore->lock);
+        semUnlock(&global_semaphore_registry->lock);
+        return -1;
+    }
 
-	if (semToClose->blockedProcesses) {
-		queueFree(semToClose->blockedProcesses);
-		semToClose->blockedProcesses = NULL;
-	}
+    // Clean up blocked processes queue
+    if (target_semaphore->blockedProcesses != NULL) {
+        queueFree(target_semaphore->blockedProcesses);
+        target_semaphore->blockedProcesses = NULL;
+    }
 
-	semUnlock(&semToClose->lock);
+    semUnlock(&target_semaphore->lock);
+    mm_free(target_semaphore);
+    semUnlock(&global_semaphore_registry->lock);
 
-	mm_free(semToClose);
-
-	semUnlock(&sQueue->lock);
-
-	return 0;
+    return 0;
 }
 
 
-int semPost(Sem semToPost) {
-	int toReturn = -1;
-	if (sQueue == NULL) {
-		return -1;
-	}
-	if (validSem(semToPost)) {
-		semLock(&semToPost->lock);
-		
-		if (queueSize(semToPost->blockedProcesses) != 0) {
-			int pid;
-			if (dequeue(semToPost->blockedProcesses, &pid) != NULL) {
-				Process *proc = get_process(pid);
-				if (proc != NULL) {
-					unblock_process(proc);
-					toReturn = 0;
-				} else {
-					toReturn = 0;
-				}
-			}
-		} else {
-			semToPost->value++;
-			toReturn = 0;
-		}
-		semUnlock(&semToPost->lock);
-	}
-	return toReturn;
-}
+int semPost(Sem target_semaphore) {
+    if (global_semaphore_registry == NULL) {
+        return -1;
+    }
+    
+    if (!is_semaphore_valid(target_semaphore)) {
+        return -1;
+    }
 
-int semWait(Sem semToWait) {
-	int toReturn = -1;
-	int blocked = 0;
-	if (sQueue == NULL) {
-		return -1;
-	}
-	if (validSem(semToWait)) {
-		semLock(&semToWait->lock);
-		
-		if (semToWait->value == 0) {
-			Process *current = get_current_process();
-			
-            if (current == NULL) {
-                semUnlock(&semToWait->lock);
-                toReturn = -1;
-            } else {
-                int pid = get_pid(current);
-
-                if (enqueue(semToWait->blockedProcesses, &pid) == NULL) {
-                    semUnlock(&semToWait->lock);
-                    toReturn = -1;
-                } else {
-                    semUnlock(&semToWait->lock);
-                    __sync_synchronize(); 
-                    
-                    block_process(current);
-                    blocked = 1;
-                    toReturn = 0;
-                }
+    semLock(&target_semaphore->lock);
+    
+    int result = -1;
+    int blocked_count = queueSize(target_semaphore->blockedProcesses);
+    
+    if (blocked_count > 0) {
+        // Unblock a waiting process
+        int unblocked_pid;
+        if (dequeue(target_semaphore->blockedProcesses, &unblocked_pid) != NULL) {
+            Process *process_to_unblock = get_process(unblocked_pid);
+            if (process_to_unblock != NULL) {
+                unblock_process(process_to_unblock);
             }
-		} else {
-			semToWait->value--;
-			toReturn = 0;
-			semUnlock(&semToWait->lock);
-		}
-		if (blocked) {
-			_force_scheduler_interrupt();
-		}
-	}
-	return toReturn;
+            result = 0;
+        }
+    } else {
+        // No blocked processes, increment semaphore value
+        target_semaphore->value++;
+        result = 0;
+    }
+    
+    semUnlock(&target_semaphore->lock);
+    return result;
 }
 
-int semGetValue(Sem semToGet) {
-	int toReturn = -1;
-	if (sQueue == NULL) {
-		return -1;
-	}
-	if (validSem(semToGet)) {
-		semLock(&semToGet->lock);
-		toReturn = semToGet->value;
-		semUnlock(&semToGet->lock);
-	}
-	return toReturn;
+int semWait(Sem target_semaphore) {
+    if (global_semaphore_registry == NULL) {
+        return -1;
+    }
+    
+    if (!is_semaphore_valid(target_semaphore)) {
+        return -1;
+    }
+
+    semLock(&target_semaphore->lock);
+    
+    int operation_result = -1;
+    int was_blocked = 0;
+    
+    if (target_semaphore->value == 0) {
+        // Semaphore value is zero, need to block current process
+        Process *current_process = get_current_process();
+        
+        if (current_process == NULL) {
+            semUnlock(&target_semaphore->lock);
+            return -1;
+        }
+        
+        int current_pid = get_pid(current_process);
+        
+        if (enqueue(target_semaphore->blockedProcesses, &current_pid) == NULL) {
+            semUnlock(&target_semaphore->lock);
+            return -1;
+        }
+        
+        semUnlock(&target_semaphore->lock);
+        __sync_synchronize();
+        
+        block_process(current_process);
+        was_blocked = 1;
+        operation_result = 0;
+    } else {
+        // Semaphore value > 0, decrement and continue
+        target_semaphore->value--;
+        operation_result = 0;
+        semUnlock(&target_semaphore->lock);
+    }
+    
+    if (was_blocked) {
+        _force_scheduler_interrupt();
+    }
+    
+    return operation_result;
 }
 
-int semGetUsersCount(Sem semToGet) {
-	int toReturn = -1;
-	if (sQueue == NULL) {
-		return -1;
-	}
-	if (validSem(semToGet)) {
-		semLock(&semToGet->lock);
-		toReturn = semToGet->users;
-		semUnlock(&semToGet->lock);
-	}
-	return toReturn;
+int semGetValue(Sem target_semaphore) {
+    if (global_semaphore_registry == NULL) {
+        return -1;
+    }
+    
+    if (!is_semaphore_valid(target_semaphore)) {
+        return -1;
+    }
+
+    semLock(&target_semaphore->lock);
+    int current_value = target_semaphore->value;
+    semUnlock(&target_semaphore->lock);
+    
+    return current_value;
 }
 
-int semGetBlockedProcessesCount(Sem semToGet) {
-	int toReturn = -1;
-	if (sQueue == NULL) {
-		return -1;
-	}
-	if (validSem(semToGet)) {
-		semLock(&semToGet->lock);
-		toReturn = queueSize(semToGet->blockedProcesses);
-		semUnlock(&semToGet->lock);
-	}
-	return toReturn;
+int semGetUsersCount(Sem target_semaphore) {
+    if (global_semaphore_registry == NULL) {
+        return -1;
+    }
+    
+    if (!is_semaphore_valid(target_semaphore)) {
+        return -1;
+    }
+
+    semLock(&target_semaphore->lock);
+    int user_count = target_semaphore->users;
+    semUnlock(&target_semaphore->lock);
+    
+    return user_count;
 }
 
-int removeFromSemaphore(Sem s, int pid) {
-	if (s == NULL) {
-		return -1;
-	}
-	semLock(&s->lock);
-	int result = queueRemove(s->blockedProcesses, &pid) == NULL ? -1 : 1;
-	semUnlock(&s->lock);
-	return result;
+int semGetBlockedProcessesCount(Sem target_semaphore) {
+    if (global_semaphore_registry == NULL) {
+        return -1;
+    }
+    
+    if (!is_semaphore_valid(target_semaphore)) {
+        return -1;
+    }
+
+    semLock(&target_semaphore->lock);
+    int blocked_count = queueSize(target_semaphore->blockedProcesses);
+    semUnlock(&target_semaphore->lock);
+    
+    return blocked_count;
+}
+
+int removeFromSemaphore(Sem target_semaphore, int process_id) {
+    if (target_semaphore == NULL) {
+        return -1;
+    }
+    
+    semLock(&target_semaphore->lock);
+    int removal_result = (queueRemove(target_semaphore->blockedProcesses, &process_id) == NULL) ? -1 : 1;
+    semUnlock(&target_semaphore->lock);
+    
+    return removal_result;
 }
 
 /**
- * Busca un semáforo por nombre en la cola global.
- * PRECONDICIÓN: El caller debe tener adquirido sQueue->lock
+ * Locates a semaphore by its name in the global registry.
+ * PRECONDITION: The caller must have acquired global_semaphore_registry->lock
  * 
- * @param name Nombre del semáforo a buscar
- * @return Puntero al semáforo si existe, NULL en caso contrario
+ * @param semaphore_name Name of the semaphore to locate
+ * @return Pointer to the semaphore if found, NULL otherwise
  */
-static Sem findSemByName(const char *name) {
-	// Validación de entrada
-	if (name == NULL) {
-		return NULL;
-	}
-	
-	int found = 0;
-	int size = queueSize(sQueue->sems);
-	sem *current = NULL;  
-	
-	if (size == 0) {
-		return NULL;
-	}
-	
-	queueBeginCyclicIter(sQueue->sems);
-	for (int i = 0; i < size && !found; i++) {
-		queueNextCyclicIter(sQueue->sems, &current);
-		if (current != NULL && my_strcmp(current->name, name) == 0) {
-			found = 1;
-		}
-	}
-	
-	return found ? current : NULL;
+static Sem locate_semaphore_by_name(const char *semaphore_name) {
+    if (semaphore_name == NULL) {
+        return NULL;
+    }
+    
+    int registry_size = queueSize(global_semaphore_registry->sems);
+    if (registry_size == 0) {
+        return NULL;
+    }
+    
+    sem *semaphore_candidate = NULL;
+    int semaphore_found = 0;
+    
+    queueBeginCyclicIter(global_semaphore_registry->sems);
+    for (int index = 0; index < registry_size && !semaphore_found; index++) {
+        queueNextCyclicIter(global_semaphore_registry->sems, &semaphore_candidate);
+        if (semaphore_candidate != NULL && 
+            my_strcmp(semaphore_candidate->name, semaphore_name) == 0) {
+            semaphore_found = 1;
+        }
+    }
+    
+    return semaphore_found ? semaphore_candidate : NULL;
 }
 
-static int validSem(Sem semToValid) {
-	int valid = 0;
-	if (semToValid != NULL && sQueue != NULL) {
-		Sem aux = semToValid;
-		semLock(&sQueue->lock);
-		if (queueElementExists(sQueue->sems, &aux)) {
-			valid = 1;
-		}
-		semUnlock(&sQueue->lock);
-	}
-	return valid;
+static int is_semaphore_valid(Sem target_semaphore) {
+    if (target_semaphore == NULL || global_semaphore_registry == NULL) {
+        return 0;
+    }
+    
+    Sem semaphore_to_check = target_semaphore;
+    semLock(&global_semaphore_registry->lock);
+    int is_valid = queueElementExists(global_semaphore_registry->sems, &semaphore_to_check);
+    semUnlock(&global_semaphore_registry->lock);
+    
+    return is_valid;
 }
 
 
-static int cmpPid(void *pid1, void *pid2) { 
-    return *((int *)pid1) - *((int *)pid2); 
+static int compare_process_ids(void *pid_a, void *pid_b) {
+    int process_id_a = *((int *)pid_a);
+    int process_id_b = *((int *)pid_b);
+    return process_id_a - process_id_b;
 }
 
-static int cmpSem(void *psem1, void *psem2) {
-	Sem a = *((Sem *)psem1);
-	Sem b = *((Sem *)psem2);
-	if (a < b) return -1;
-	if (a > b) return 1;
-	return 0;
+static int compare_semaphore_pointers(void *sem_a_ptr, void *sem_b_ptr) {
+    Sem semaphore_a = *((Sem *)sem_a_ptr);
+    Sem semaphore_b = *((Sem *)sem_b_ptr);
+    
+    if (semaphore_a < semaphore_b) {
+        return -1;
+    }
+    if (semaphore_a > semaphore_b) {
+        return 1;
+    }
+    return 0;
 }
